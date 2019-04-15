aed4e670ae7cd48834f4d68fd6ce3661d211d405
PbernsY <conorberns@gmail.com>
2019-04-15 01:24:58 +0100

    commenting

---
+++ fitalgorithmspython.py
@@ -0,0 +1,167 @@
+
+# Python3 implementation of worst - Fit algorithm  
+  
+# Function to allocate memory to blocks as  
+# per worst fit algorithm  
+def worstFit(blockSize, m, processSize, n): 
+      
+    # Stores block id of the block  
+    # allocated to a process  
+      
+    # Initially no block is assigned  
+    # to any process  
+    allocation = [-1] * n 
+      
+    # pick each process and find suitable blocks  
+    # according to its size ad assign to it  
+    for i in range(n): 
+          
+        # Find the best fit block for  
+        # current process  
+        wstIdx = -1
+        for j in range(m): 
+            if blockSize[j] >= processSize[i]: 
+                if wstIdx == -1:  
+                    wstIdx = j  
+                elif blockSize[wstIdx] < blockSize[j]:  
+                    wstIdx = j 
+  
+        # If we could find a block for  
+        # current process  
+        if wstIdx != -1: 
+              
+            # allocate block j to p[i] process  
+            allocation[i] = wstIdx  
+  
+            # Reduce available memory in this block.  
+            blockSize[wstIdx] -= processSize[i] 
+  
+    print("Process No. Process Size Block no.") 
+    for i in range(n): 
+        print(i + 1, "       ",  
+              processSize[i], end = "    ")  
+        if allocation[i] != -1: 
+            print(allocation[i] + 1)  
+        else: 
+            print("Not Allocated") 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Python3 implementation of Best - Fit algorithm  
+  
+# Function to allocate memory to blocks  
+# as per Best fit algorithm  
+def bestFit(blockSize, m, processSize, n): 
+      
+    # Stores block id of the block  
+    # allocated to a process  
+    allocation = [-1] * n  
+      
+    # pick each process and find suitable  
+    # blocks according to its size ad  
+    # assign to it 
+    for i in range(n): 
+          
+        # Find the best fit block for 
+        # current process  
+        bestIdx = -1
+        for j in range(m): 
+            if blockSize[j] >= processSize[i]: 
+                if bestIdx == -1:  
+                    bestIdx = j  
+                elif blockSize[bestIdx] > blockSize[j]:  
+                    bestIdx = j 
+  
+        # If we could find a block for  
+        # current process  
+        if bestIdx != -1: 
+              
+            # allocate block j to p[i] process  
+            allocation[i] = bestIdx  
+  
+            # Reduce available memory in this block.  
+            blockSize[bestIdx] -= processSize[i] 
+  
+    print("Process No. Process Size     Block no.") 
+    for i in range(n): 
+        print(i + 1, "       ", processSize[i],  
+                                end = "      ")  
+        if allocation[i] != -1:  
+            print(allocation[i] + 1)  
+        else: 
+            print("Not Allocated") 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Function to allocate memory to  
+# blocks as per First fit algorithm  
+def firstFit(blockSize, m, processSize, n): 
+      
+    # Stores block id of the  
+    # block allocated to a process  
+    allocation = [-1] * n  
+  
+    # Initially no block is assigned to any process 
+  
+    # pick each process and find suitable blocks  
+    # according to its size ad assign to it  
+    for i in range(n): 
+        for j in range(m): 
+            if blockSize[j] >= processSize[i]: 
+                  
+                # allocate block j to p[i] process  
+                allocation[i] = j  
+  
+                # Reduce available memory in this block.  
+                blockSize[j] -= processSize[i]  
+  
+                break
+  
+    print(" Process No. Process Size      Block no.") 
+    for i in range(n): 
+        print(" ", i + 1, "      ", processSize[i],  
+                          "      ", end = " ")  
+        if allocation[i] != -1:  
+            print(allocation[i] + 1)  
+        else: 
+            print("Not Allocated") 
+  
+# Driver code  
+if __name__ == '__main__':  
+    blockSize = [100, 500, 200, 300, 600]  
+    processSize = [212, 417, 112, 426] 
+    m = len(blockSize) 
+    n = len(processSize) 
+  
+    firstFit(blockSize, m, processSize, n) 
+      
+# This code is contributed by PranchalK 
--- parser.py
+++ parser.py
@@ -9,19 +9,28 @@ import subprocess
 
 
 def set_flags(string):
+	''' This is a function to set a list of flags. These flags are then used to perform tasks
+	like background execution etc '''
 	global flags
 	global output
 	global parseable
+	# set up global variables so they can be passed to the other relevant parsing functions
 	flags = []
 	output = []
 	parseable = shlex.split(string)
 	if not len(string):
 		return
+		# this prevent us trying to index an empty string
 	if parseable[-1] == "&":
+		# we know, if the ampersand is present, it can only be the last character of the input string
+		# if thats the case, append the ampersand to the flags list
 		flags.append(parseable.pop())
 
 	if ">>" in string or ">" in string:
+		# append the redirection file name to the flags list
+
 		flags.append(parseable.pop())
+		# append the redirection symbol to the output list
 		output.append(parseable.pop())
 
 	return flags, parseable, output
@@ -47,22 +56,47 @@ def launch(args):
 
 
 def bothflags(exece, args):
+	''' this function is called when both flags are set [background_exec, outputfilename]'''
 	with open(flags[-1], checkwrite(output)) as current_file:
+		# open the file name with file mode depending on the checkwrite function
 		shellins.stdout_param = current_file
+		# modify the stdout of our shell based on file we opened
+		# spawn a process for our shell to exec 
 		shellins.spawn_subprocess(shellins.commands[exece], args)
 
 def oneflag(exece, args):
+	''' this function is called when one flag is set in our flags list'''
 	current_command = shellins.commands[exece]
-	if not len(output):
+	print(parseable)
+	print(flags)
+	print(output)
+	print(args)
+	if len(output) == 0:
+		# if the length of the output list is 0, we just have to background exec
 		if len(args):
+			# this makes sure we dont try to pass a command that takes positional arguments 0 arguments
 			shellins.spawn_subprocess(current_command, args)
 		else:
+			# do the opposite, call a single arrity command with no args
 			shellins.spawn_subprocess(current_command)
-			"this bit calls the spawn"  "this bit is the command"       "this bit is the arg"
+			#"this bit calls the spawn"  "this bit is the command"       "this bit is the arg"
 	else:
-		with open(flags[0], checkwrite(output)) as current_file:
-			shellins.__stdout__param = current_file
-			current_command(args)
+		# we have a redirection symbol but no ampersand
+		if len(args):
+			print("got here")
+			with open(flags[-1], checkwrite(output)) as current_file:
+				# open the file in the appropriate mode
+				shellins.stdout_param = current_file
+				# execute the current command with args
+				current_command(args)
+		else:
+			print("not ")
+			with open(flags[-1], checkwrite(output)) as current_file:
+				# open the file in the appropriate mode
+				shellins.stdout_param = current_file
+				# execute the current command with args
+				shellins.commands[exece]()
+
 
 		
 def checkwrite(output_list):
